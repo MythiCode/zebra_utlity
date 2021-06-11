95e20107405dfa679a351f3cebeefe2ca2ccbaea
hrshs <hrshs91@gmail.com>
2021-06-11 16:15:34 +0430

    Clean code

--- lib/zebrautility.dart
+++ lib/zebrautility.dart
@@ -5,12 +5,13 @@ class Zebrautility {
   static const MethodChannel _channel = const MethodChannel('zebrautility');
 
   static Future<ZebraPrinter> getPrinterInstance(
-      { Function(String name, String ipAddress, bool isWifi)?onPrinterFound,
-       onPrinterDiscoveryDone,
-       Function(int errorCode, String errorText)? onDiscoveryError,
-       Function(String status, String color)? onChangePrinterStatus,
-       onPermissionDenied}) async {
-    String id = await (_channel.invokeMethod("getInstance") as FutureOr<String>);
+      {Function(String name, String ipAddress, bool isWifi)? onPrinterFound,
+      onPrinterDiscoveryDone,
+      Function(int errorCode, String errorText)? onDiscoveryError,
+      Function(String status, String color)? onChangePrinterStatus,
+      onPermissionDenied}) async {
+    String id =
+        await (_channel.invokeMethod("getInstance") as FutureOr<String>);
     ZebraPrinter printer = ZebraPrinter(id, onPrinterFound,
         onPrinterDiscoveryDone, onDiscoveryError, onChangePrinterStatus,
         onPermissionDenied: onPermissionDenied);
